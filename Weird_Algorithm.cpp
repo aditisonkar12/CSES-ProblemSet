368400934a3e77e3b45bc354072ccaf3f5336616
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-12-13 16:23:36 +0530

    updated

--- Weird_Algorithm.cpp
+++ Weird_Algorithm.cpp
@@ -44,6 +44,18 @@ void input(vector<T> &arr, int n)
 
 void solve()
 {
+    int n;
+    cin>>n;
+    vector<int> ans;
+    ans.push_back(n);
+    while(n!=1){
+        if(n%2==0) n/=2;
+        else n=n*3+1;
+        ans.push_back(n);
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
 }
 
 int main()
