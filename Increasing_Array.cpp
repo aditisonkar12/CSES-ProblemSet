a4c1984f428a5f66b2405f17b929a748cbe3344e
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-12-13 17:13:20 +0530

    updated

---
+++ Increasing_Array.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <bitset>
+#include <iomanip>
+#include <numeric>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+template <typename T>
+void input(vector<T> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void solve()
+{
+    long long n;
+    cin>>n;
+    vector<long long> arr(n);
+    input(arr,n);
+    long long cnt=0;
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            cnt+=arr[i-1]-arr[i];
+            arr[i]=arr[i-1];
+        }
+    }
+    cout<<cnt;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+    return 0;
+}
