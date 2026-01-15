d58e801b777c55e17c3f6d32f960cab084a37005
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-01-15 14:24:17 +0530

    updated

--- Concert_Tickets.cpp
+++ Concert_Tickets.cpp
@@ -44,7 +44,27 @@ void input(vector<T> &arr, int n)
 
 int solve()
 {
-    
+    int n,m;
+    cin>>n>>m;
+    multiset<int> tickets;
+    for(int i=0;i<n;i++){
+        int price;
+        cin>>price;
+        tickets.insert(price);
+    }
+
+    for(int i=0;i<m;i++){
+        int budget;
+        cin>>budget;
+        auto it = tickets.upper_bound(budget);
+        if(it==tickets.begin()){
+            cout<<-1<<"\n";
+        }else{
+            --it;
+            cout<<*it<<"\n";
+            tickets.erase(it);
+        }
+    }
 }
 
 int main()
