22dc1d6cc4e89304baeb98a989fde8209f2614b4
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-01-17 12:21:38 +0530

    updated

--- Counting_Bits.cpp
+++ Counting_Bits.cpp
@@ -34,29 +34,34 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
 template <typename T>
-void input(vector<T> &arr, int n) {
-     for (int i = 0; i < n; i++) {
-         cin >> arr[i];
-     }
-}
-
-void solve() {
-    long long n;
-    cin>>n;
-
-    long long ans=0;
-    for(int i=0;(1LL<<i)<=n;i++){
-        long long x=1LL<<(i+1);
-        long long div=(n+1)/x;
-        long long rem=(n+1)%x;
-
-        ans+=div*(1LL<<i);
-        ans+=max(0LL,rem-(1LL<<i));
+void input(vector<T> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<ans<<"\n";
 }
 
-int main() {
+void solve()
+{
+    long long n;
+    cin >> n;
+
+    long long ans = 0;
+    for (int i = 0; (1LL << i) <= n; i++)
+    {
+        long long x = 1LL << (i + 1);
+        long long div = (n + 1) / x;
+        long long rem = (n + 1) % x;
+
+        ans += div * (1LL << i);
+        ans += max(0LL, rem - (1LL << i));
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
 
